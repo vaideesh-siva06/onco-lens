a68e6fc6f3ba7f777714bab972d2c5410dda00c0
Vaideesh Siva <vaideeshwarsiva2006@gmail.com>
2026-02-10 00:17:17 -0600

    Update main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://onco-lens-sxrc.onrender.com/"],        
+    allow_origins=["https://onco-lens-sxrc.onrender.com"],        
     allow_credentials=True,
     allow_methods=["*"],        
     allow_headers=["*"],        
