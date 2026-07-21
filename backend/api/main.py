e73d334de898775045b0c744f5e31a28899fd6f8
Vaideesh-siv <vaideesh.siv@gmail.com>
2026-07-20 22:53:13 -0500

    python cors error fixed

--- backend/api/main.py
+++ backend/api/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost", "http://localhost:80", "https://onco-lens-sxrc.onrender.com", "https://onco-lens.vaideesh4.workers.dev"],  # exact frontend URL
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
