43be574f7aad4d501f9231c0a18d1c50ffa28091
Vaideesh-siv <vaideesh.siv@gmail.com>
2026-07-21 00:27:44 -0500

    oncolens ai message removed

--- backend/api/model/convert_model_to_tflite.py
+++ backend/api/model/convert_model_to_tflite.py
@@ -1,16 +1,17 @@
 import tensorflow as tf
 
-model = tf.keras.models.load_model(
-    "./cancer_model.keras",
-    compile=False
-)
+model = tf.keras.models.load_model("cancer_model.keras")
 
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+# Add this
+converter.target_spec.supported_ops = [
+    tf.lite.OpsSet.TFLITE_BUILTINS
+]
 
 tflite_model = converter.convert()
 
-with open("./cancer_model.tflite", "wb") as f:
+with open("cancer_model.tflite", "wb") as f:
     f.write(tflite_model)
 
 print("Done!")
